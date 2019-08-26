3fd329bb7740e2f3c78a9397bd02c01b8cde582d
Ronak Shastri <ronak.shastri@scotiabank.com>
2019-08-26 17:02:02 -0400

    add lables and a button for the demo.

--- CoordinatorTest/Viewcontrollers/ViewController3.swift
+++ CoordinatorTest/Viewcontrollers/ViewController3.swift
@@ -10,21 +10,53 @@ import UIKit
 
 class ViewController3: UIViewController {
     
-    var headerView : UIView!
+    var headerView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+ 
+    
+    var titleLable : UILabel = {
+        let label = UILabel()
+        label.text = "ViewController 3"
+        return label
+    }()
+    var goToButton : UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Get Lost", for: .normal)
+        btn.setTitle("Don't push,Going..", for: .focused)
+        return btn
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        headerView = UIView()
-        headerView.backgroundColor = .yellow
+        // Do any additional setup after loading the view, typically from a nib.        
         self.view.addSubview(headerView)
+        self.view.addSubview(titleLable)
+        self.view.addSubview(goToButton)
         
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier : 1).isActive = true
-        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
+        setConstrains()
     }
-    
+
+    func setConstrains() {
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        titleLable.translatesAutoresizingMaskIntoConstraints = false
+        goToButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            headerView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor),
+            
+            titleLable.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            titleLable.topAnchor.constraint(equalTo: self.headerView.topAnchor, constant: 100),
+            
+            goToButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            goToButton.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+            goToButton.heightAnchor.constraint(equalToConstant: 100),
+            goToButton.widthAnchor.constraint(equalToConstant: 100)
+            ])
+    }
+
 }
